f4c5f183366d0f0bc9168e7fb64f3ce1cfffcb26
Hansisunstoppable <70207220+Hansisunstoppable@users.noreply.github.com>
2023-12-29 21:33:10 +0800

    test1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,9 @@
 import { defaultTheme } from 'vuepress'
 
 export default {
-  base: "/HansBlog/",
   lang: 'zh-CN',
-  title: '方行的博客',
-  description: '欢迎来到方行的博客',
+  title: 'zhanyd的博客',
+  description: '欢迎来到zhanyd的博客',
   theme: defaultTheme({
     // 在这里进行配置
     navbar: [
@@ -14,40 +13,17 @@ export default {
         link: '/',
       },
       {
-        text: '算法',
-        link: '/Algorithm/',
-      },
-      {
-        text: '数据结构',
-        link: '/DataStructure/',
-      },
-      {
-        text: '计算机网络',
+        text: '网络',
         link: '/network/',
       },
       {
-        text: '操作系统',
-        link: '/OperatingSystem/',
+        text: '设计模式',
+        link: '/designPatterns/',
       },
     ],
     // 侧边栏对象
     // 不同子路径下的页面会使用不同的侧边栏
     sidebar: {
-      '/Algorithm/': [
-        {
-          text: '算法',
-          children: [
-              {
-                text: '第 46 届 ICPC 国际大学生程序设计竞赛亚洲区域赛（上海）--- I-Steadily Growing Steam（背包dp）',
-                link: '/network/第 46 届 ICPC 国际大学生程序设计竞赛亚洲区域赛（上海）--- I-Steadily Growing Steam（背包dp）.md',
-              },
-              {
-                text: '在Jekyll中创建一个新的列表页面',
-                link: '/network/在Jekyll中创建一个新的列表页面.md',
-              }
-            ],
-        },
-      ],
       '/network/': [
         {
           text: '网络',
@@ -63,7 +39,6 @@ export default {
             ],
         },
       ],
-      
       '/designPatterns/': [
         {
           text: '设计模式',
